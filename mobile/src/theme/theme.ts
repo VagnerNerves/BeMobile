d7868e9e9c7e2a57334a1e41be030826bcd7f90b
Vagner Nerves <vagnernervessantos@gmail.com>
2024-11-20 10:56:56 -0300

    feat: Create theme.

---
+++ mobile/src/theme/theme.ts
@@ -0,0 +1,35 @@
+export const theme = {
+  COLORS: {
+    BLUE: {
+      PRIMARY: '#0500FF',
+      '10': '#EDEFFB'
+    },
+    BLACK: {
+      NEUTRAL: '#1C1C1C'
+    },
+    WHITE: {
+      NEUTRAL: '#FFFFFF'
+    },
+    GRAY: {
+      '00': '#F0F0F0',
+      '05': '#F5F5F5',
+      '10': '#DFDFDF',
+      '20': '#9E9E9E'
+    }
+  },
+  FONTS: {
+    HELVETICA_REGULAR: 'HelveticaNeue_400Regular',
+    HELVETICA_MEDIUM: 'HelveticaNeue_500Medium',
+    HELVETICA_BOLD: 'HelveticaNeue_700Bold',
+    TEXT_STYLE: {
+      '16': {
+        SIZE: 16,
+        LINEHEIGHT: 19.09
+      },
+      '20': {
+        SIZE: 20,
+        LINEHEIGHT: 24.42
+      }
+    }
+  }
+} as const
