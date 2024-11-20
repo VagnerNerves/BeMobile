7e53b01cadaf46f9052ec737f5c7de0e89d8cd0f
Vagner Nerves <vagnernervessantos@gmail.com>
2024-11-20 12:46:05 -0300

    feat: Ajuste theme provider and bottom navigation bar.

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,11 +1,16 @@
 import { useEffect } from 'react'
 
-import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
+import * as NavigationBar from 'expo-navigation-bar'
+
+import { Stack } from 'expo-router'
+import { ThemeProvider, DefaultTheme } from '@react-navigation/native'
 
 import { useFonts } from 'expo-font'
 
 import { Loading } from '@/src/components/Loading'
+import { theme } from '../theme/theme'
+import { Platform, StatusBar } from 'react-native'
 
 SplashScreen.preventAutoHideAsync()
 
@@ -16,15 +21,36 @@ export default function Layout() {
     HelveticaNeue_700Bold: require('@/assets/fonts/HelveticaNeue_700Bold.otf')
   })
 
+  const themeProvider = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: theme.COLORS.WHITE.NEUTRAL
+    }
+  }
+
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync()
     }
   }, [loaded])
 
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      NavigationBar.setPositionAsync('absolute')
+      NavigationBar.setBackgroundColorAsync('#ffffff00')
+    }
+  })
+
   return (
-    <>
+    <ThemeProvider value={themeProvider}>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       {loaded ? <Stack screenOptions={{ headerShown: false }} /> : <Loading />}
-    </>
+    </ThemeProvider>
   )
 }
