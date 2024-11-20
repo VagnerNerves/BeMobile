945f4f1d6fe43300382ab2284c2d50772f73993d
Vagner Nerves <vagnernervessantos@gmail.com>
2024-11-20 11:26:14 -0300

    feat: loaded fonts on app

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,5 +1,30 @@
+import { useEffect } from 'react'
+
 import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+
+import { useFonts } from 'expo-font'
+
+import { Loading } from '@/src/components/Loading'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />
+  const [loaded] = useFonts({
+    HelveticaNeue_400Regular: require('@/assets/fonts/HelveticaNeue_400Regular.otf'),
+    HelveticaNeue_500Medium: require('@/assets/fonts/HelveticaNeue_500Medium.otf'),
+    HelveticaNeue_700Bold: require('@/assets/fonts/HelveticaNeue_700Bold.otf')
+  })
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+  return (
+    <>
+      {loaded ? <Stack screenOptions={{ headerShown: false }} /> : <Loading />}
+    </>
+  )
 }
--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,25 @@
 import { Text, View } from 'react-native'
+import { theme } from '@/src/theme/theme'
 
 export default function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Initial Project BeMobile</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: theme.FONTS.HELVETICA_REGULAR,
+          fontSize: theme.FONTS.TEXT_STYLE[16].SIZE,
+          lineHeight: theme.FONTS.TEXT_STYLE[16].LINEHEIGHT,
+          color: theme.COLORS.BLACK.NEUTRAL
+        }}
+      >
+        Initial Project BeMobile
+      </Text>
     </View>
   )
 }
